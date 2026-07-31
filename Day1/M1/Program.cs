21201acb53abc1333d5c7e2b47045cf2e2dca2a5
kstwkiv <sathwikakurma1@gmail.com>
2026-07-31 10:14:01 +0530

    Initial commit

---
+++ Day1/M1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double price;
+        int quantity;
+        double discount;
+
+        Console.WriteLine("Enter item price: ");
+
+        if(!double.TryParse(Console.ReadLine(),out price))
+        {
+            Console.WriteLine("Invalid Price eneterd!!");
+            return;
+        }
+
+        Console.WriteLine("Enter the quantity:");
+        if(!int.TryParse(Console.ReadLine(),out quantity))
+        {
+            Console.WriteLine("Invalid quantity entered!!");
+            return;
+
+        }
+
+        Console.WriteLine("Enter discount percentage: ");
+        if(!double.TryParse(Console.ReadLine(),out discount))
+        {
+            Console.WriteLine("Invalid dicount percentage enetered!!");
+            return;
+             
+        }
+
+        if(price<0 || quantity <=0 || discount < 0)
+        {
+            Console.WriteLine("Invalid input values!!");
+            return;
+        }
+
+        double subtotal = price * quantity;
+        double discountAmount = subtotal * (discount / 100);
+        double finalAmount = subtotal - discountAmount;
+
+        Console.WriteLine("\n-------BILL-------");
+        Console.WriteLine("Subtotal: "+Math.Round(subtotal,2));
+        Console.WriteLine("Discount: " + Math.Round(discountAmount, 2));
+        Console.WriteLine("Final Amount: " + Math.Round(finalAmount, 2));
+            
+    }
+}
